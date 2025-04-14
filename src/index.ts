93b9a505ea8803cdd7f19dd16dfbc798179c0305
Omer Hadar <omer.hadar14@gmail.com>
2025-04-14 23:35:51 +0300

    Add Reputation interface and export from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./interfaces/User";
 export * from "./interfaces/Filters";
 export * from "./interfaces/Product";
+export * from "./interfaces/Reputation";
---
+++ src/interfaces/Reputation.ts
@@ -0,0 +1,6 @@
+export interface Reputation {
+  userId: string;
+  rating: number;
+  feedback?: string;
+  date: Date;
+}
