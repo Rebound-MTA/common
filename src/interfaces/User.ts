5fba830074e51c48d2a2392bbabc8061bddef951
Omer Hadar <omer.hadar14@gmail.com>
2025-04-17 13:49:06 +0300

    Add Location interface and update User interface to include location and optional fields

--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -1,14 +1,17 @@
 import { Filters } from "./Filters";
-
+import { Location } from "./Location";
 export interface User {
   id: string;
   fullName: string;
   email: string;
+  phoneNumber?: string;
+  bio?: string;
   password: string;
   role?: string[];
   profile?: {
     avatarUrl?: string;
   };
+  location?: Location;
   preferences?: Filters[];
   createdAt?: Date;
   updatedAt?: Date;
