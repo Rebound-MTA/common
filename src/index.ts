937caeeada877a6962ed895b6eab8874445d97f3
Omer Hadar <omer.hadar14@gmail.com>
2025-04-17 13:56:32 +0300

    Bump version to 1.0.0-beta7 in package.json and export Location interface from index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./interfaces/User";
 export * from "./interfaces/Filters";
 export * from "./interfaces/Product";
 export * from "./interfaces/Reputation";
+export * from "./interfaces/Location";
